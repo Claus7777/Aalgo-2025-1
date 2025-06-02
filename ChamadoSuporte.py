9a489ccbc58c64144994d251943274a9d728438a
claus7777 <lucasmartinsoliveira95@gmail.com>
2025-06-02 16:19:41 -0300

    Commit inicial. Classe ChamadoSuporte implementada com funcoes de serialização/deserialização, calcular_prioridade_total e contructor.

---
+++ ChamadoSuporte.py
@@ -0,0 +1,80 @@
+from enum import Enum
+from datetime import datetime
+
+class tipoCliente(Enum):
+    PRIORITARIO = 1
+    SEM_PRIORIDADE = 2
+    DEMONSTRACAO = 3
+
+class tipoChamado(Enum):
+    SERVER_DOWN = 1
+    IMPACTA_PRODUCAO = 2
+    SEM_IMPACTO = 3
+    DUVIDA = 4
+
+class ChamadoSuporte:
+
+    def __init__(
+        self,
+        id_chamado: int,
+        cliente_nome: str,
+        tipo_cliente: tipoCliente,
+        tipo_chamado: tipoChamado,
+        descricao: str,
+        timestamp: datetime
+    ):
+        self.id_chamado = id_chamado
+        self.cliente_nome = cliente_nome
+        self.descricao = descricao
+        self.timestamp = timestamp
+
+        #Ve se o tipo_cliente está no enum tipoCliente
+        if isinstance(tipo_cliente, TipoCliente):
+            self.tipo_cliente = tipo_cliente
+        elif tipo_cliente in [item.value for item in TipoCliente]:
+            self.tipo_cliente = TipoCliente(tipo_cliente)
+        else:
+            valid_options = ', '.join([item.name for item in TipoCliente])
+            raise ValueError(f"Tipo de cliente inválido: {tipo_cliente}. Opções: {valid_options}")
+
+        #Mesma coisa com o tipo_chamado
+        if isinstance(tipo_chamado, TipoChamado):
+            self.tipo_chamado = tipo_chamado
+        elif tipo_chamado in [item.value for item in TipoChamado]:
+            self.tipo_chamado = TipoChamado(tipo_chamado)
+        else:
+            valid_options = ', '.join([item.name for item in TipoChamado])
+            raise ValueError(f"Tipo de chamado inválido: {tipo_chamado}. Opções: {valid_options}")
+    
+    def calcular_prioridade_total(self):
+        """Retorn tuple com (tipo_chamado, tipo_cliente)"""
+        prioridade_total = (self.tipo_chamado, self.tipo_cliente)
+        return prioridade_total
+    
+    def to_dict(self) -> dict:
+        """"Serializa chamado para dicionário pra facilitar a conversão pra JSON quando formos fazer a API"""
+        return {
+            "id_chamado": self.id_chamado,
+            "cliente_nome": self.cliente_nome,
+            "tipo_cliente": self.tipo_cliente.name,
+            "tipo_chamado": self.tipo_chamado.name,
+            "descricao": self.descricao,
+            "timestamp": self.timestamp.isoformat(),
+            "prioridades": {
+                "cliente": self.prioridade_cliente,
+                "chamado": self.prioridade_chamado,
+                "total": self.prioridade_total
+            }
+        }
+    
+    def from_dict(cls, data: dict) -> dict:
+        """Deserializa de dicionário"""
+        return cls(
+            id_chamado = data['id_chamado'],
+            cliente_nome = data['cliente_nome'],
+            tipo_cliente = data['tipo_cliente'],
+            tipo_chamado = data['tipo_chamado'],
+            descricao = data['descricao'],
+            timestamp = datetime.fromisoformat(data['timestamp'])
+        )
+
