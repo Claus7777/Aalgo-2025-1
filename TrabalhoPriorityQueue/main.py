ccb9df74214d37872edb2b8cbb698988019baa4e
T0D_05 <thomoliv2004@gmail.com>
2025-06-03 18:21:07 -0300

    primeiro commit para o rest

--- TrabalhoPriorityQueue/main.py
+++ TrabalhoPriorityQueue/main.py
@@ -1,5 +1,7 @@
 import heapq
 from plyer import notification
+from fastapi import FastAPI
+import ChamadoSuporte
 
 class PriorityQueue:
     def __init__(self):
@@ -8,7 +10,7 @@ class PriorityQueue:
     def push(self, prioridade_combinada_tuple, timestamp, chamado_obj):
         heapq.heappush(self._heap, (prioridade_combinada_tuple, timestamp, chamado_obj))
 
-        self._show_notification("Novo chamado adicionado", "Resolva!")
+        self._enviar_notificacao_desktop("Novo chamado adicionado", "Resolva!")
 
     def pop(self):
         return heapq.heappop(self._heap)[-1]
@@ -16,8 +18,8 @@ class PriorityQueue:
     def is_empty(self):
         return len(self._heap) == 0
     
-    def _show_notification(titulo, mensagem):
-        """Mostra notificaçõesn no desktop utilizando plyer"""
+    def _enviar_notificacao_desktop(self, titulo, mensagem):
+        """Mostra notificações no desktop utilizando plyer"""
         try:
             notification.notify(
                 title = titulo,
@@ -28,5 +30,55 @@ class PriorityQueue:
         except Exception as e:
             print(f"Falha na notificação: {e}")
     
+    def processar_proximo_chamado(self):
+        if not self.is_empty():
+            chamado = self.pop()
+            print(f"Processando chamado: {chamado.to_dict()}")
+            # Extrai prioridade_combinada_tuple, timestamp e chamado_objeto da tupla recuperada.
+            prioridade_combinada_tuple, timestamp, chamado_objeto = chamado
+            processarChamado(chamado_objeto)
+            # imprima
+        else:
+            print("Nenhum chamado na fila.")
+
+app = FastAPI()
 pq = PriorityQueue()
 
+@app.post("/chamado")
+def adicionar_chamado(chamado_obj: ChamadoSuporte):
+    """
+    Adiciona um chamado à fila de prioridade.
+    Exemplo de uso: POST /chamado com o corpo do chamado.
+    """
+    prioridade_combinada_tuple = chamado_obj.calcular_prioridade_total()
+    timestamp = chamado_obj.timestamp
+    pq.push(prioridade_combinada_tuple, timestamp, chamado_obj)
+    return {"message": "Chamado adicionado com sucesso!"}
+
+@app.get("/proximo_chamado")
+def processar_proximo_chamado():
+    """
+    Processa o próximo chamado na fila de prioridade.
+    Exemplo de uso: GET /processar_proximo_chamado
+    """
+    pq.processar_proximo_chamado()
+    return {"message": "Próximo chamado processado!"}
+
+def processarChamado(chamado_obj):
+    tipo_chamado_map = {
+        "SERVER_DOWN": "Chamado de servidor fora do ar",
+        "IMPACTA_PRODUCAO": "Chamado que impacta a produção",
+        "SEM_IMPACTO": "Chamado sem impacto",
+        "DUVIDA": "Chamado de dúvida",
+    }
+
+
+    if chamado_obj.tipo_chamado in tipo_chamado_map:
+        pq._enviar_notificacao_desktop(
+            tipo_chamado_map[chamado_obj.tipo_chamado],
+            f"""Cliente 
+            {chamado_obj.cliente_nome} 
+            ({chamado_obj.tipo_cliente})\nTipo:
+            {chamado_obj.tipo_chamado}\nDescrição:
+            {chamado_obj.descricao[:50]}...""",
+        )
