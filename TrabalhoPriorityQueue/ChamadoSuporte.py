5c247451abf2d99bbb37723ba38a291bddfb1546
Lucas Martins <44577287+Claus7777@users.noreply.github.com>
2025-06-26 23:32:17 -0300

    Adicionada tempo estimado de resolução por tipo de chamado, além da capacidade de serializar e desserializar tanto o tempo de chamado quanto a prioridade escalada

--- TrabalhoPriorityQueue/ChamadoSuporte.py
+++ TrabalhoPriorityQueue/ChamadoSuporte.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from pydantic import BaseModel
 
@@ -37,6 +37,7 @@ class ChamadoSuporte:
         self.cliente_nome = cliente_nome
         self.descricao = descricao
         self.timestamp = timestamp
+        self.prioridade_escalada = None
 
         #Ve se o tipo_cliente está no enum tipoCliente
         if isinstance(tipo_cliente, tipoCliente):
@@ -55,11 +56,38 @@ class ChamadoSuporte:
         else:
             valid_options = ', '.join([item.name for item in tipoChamado])
             raise ValueError(f"Tipo de chamado inválido: {tipo_chamado}. Opções: {valid_options}")
+        
+        self.tempo_estimado_resolucao = self._definir_tempo_estimado()
+        
+        
+    def _definir_tempo_estimado(self) -> timedelta:
+        """Define tempo de resolução com base no tipo de chamado"""
+        if self.tipo_chamado == tipoChamado.SERVER_DOWN:
+            return timedelta(hours=1)
+        elif self.tipo_chamado == tipoChamado.IMPACTA_PRODUCAO:
+            return timedelta(hours=4)
+        elif self.tipo_chamado == tipoChamado.SEM_IMPACTO:
+            return timedelta(hours=24)
+        elif self.tipo_chamado == tipoChamado.DUVIDA:
+            return timedelta(hours=48)
+        return timedelta(hours=72)
     
     def calcular_prioridade_total(self):
-        """Retorn tuple com (tipo_chamado, tipo_cliente)"""
+        """Retorna tuple com (tipo_chamado, tipo_cliente)"""
+        if self.prioridade_escalada is not None:
+            return self.prioridade_escalada
         prioridade_total = (self.tipo_chamado.value, self.tipo_cliente.value)
         return prioridade_total
+    
+    def escala_prioridade(self, novo_tipo_chamado = None, novo_tipo_cliente = None):
+        """Escalar manualmente a prioridade de um chamado"""
+        if novo_tipo_chamado is None:
+            novo_tipo_chamado = self.tipo_chamado
+        
+        if novo_tipo_cliente is None:
+            novo_tipo_cliente = self.tipo_cliente
+        self.prioridade_escalada = (novo_tipo_chamado.value, novo_tipo_cliente.value)
+
     
     def to_dict(self) -> dict:
         """"Serializa chamado para dicionário pra facilitar a conversão pra JSON quando formos fazer a API"""
@@ -70,11 +98,13 @@ class ChamadoSuporte:
             "tipo_chamado": self.tipo_chamado.value,
             "descricao": self.descricao,
             "timestamp": self.timestamp.isoformat(),
+            "prioridade_escalada": self.prioridade_escalada,
+            "tempo_estimado_resolucao": self.tempo_estimado_resolucao.total_seconds()
         }
     
     def from_dict(cls, data: dict) -> dict:
         """Deserializa de dicionário"""
-        return cls(
+        chamado = cls(
             id_chamado = data['id_chamado'],
             cliente_nome = data['cliente_nome'],
             tipo_cliente = data['tipo_cliente'],
@@ -82,3 +112,8 @@ class ChamadoSuporte:
             descricao = data['descricao'],
             timestamp = datetime.fromisoformat(data['timestamp'])
         )
+        if data.get("prioridade_escalada"):
+            chamado.prioridade_escalada = tuple(data["prioridade_escalada"])
+        if data.get("tempo_estimado_resolucao"):
+            chamado.tempo_estimado_resolucao = timedelta(seconds=float(data["tempo_estimado_resolucao"]))
+        return chamado
